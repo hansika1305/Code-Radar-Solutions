4ad5cb3c2cf45344b3ec6abc6216109732405fd2
Hansika Khanna <hansika1305.becse24@chitkara.edu.in>
2025-02-11 01:01:08 +0530

    Solution to Check Profit or Loss

---
+++ Check Profit or Loss@@6788dd63490637cfc44e1b98/code.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main() {
+    int a;
+    int b;
+    scanf("%d",&a);
+    scanf("%d",&b);
+
+    if(sp>cp){
+        printf("Profit");
+    }
+    else if(cp>sp){
+        printf("Loss");
+    }
+    else{
+        printf("NO Profit No Loss");
+    }
+    return 0;
+}
